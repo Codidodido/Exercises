e3e077ee41de52cf291c5852c10d7bb946276551
Codidodido <139787880+Codidodido@users.noreply.github.com>
2023-11-29 14:03:42 +0330

    Create static abilities class script
    -> Create all abilites in abilites class

---
+++ Object Oriented Programming/Abilities/Abilities/Abilities.cs
@@ -0,0 +1,9 @@
+namespace Abilities
+{
+    public static class Abilities
+    {
+        public static AttackAbility AttackAbility = new AttackAbility();
+        public static HealAbility HealAbility = new HealAbility();
+        public static MagicAbility MagicAbility = new MagicAbility();
+    }
+}
