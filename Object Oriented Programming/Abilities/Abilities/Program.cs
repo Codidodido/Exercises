b8112055e608e661f53973bb7496741ad598dff1
Codidodido <139787880+Codidodido@users.noreply.github.com>
2023-11-29 14:04:08 +0330

    Edit program class script
    -> Write a simple test

--- Object Oriented Programming/Abilities/Abilities/Program.cs
+++ Object Oriented Programming/Abilities/Abilities/Program.cs
@@ -6,6 +6,13 @@ namespace Abilities
     {
         public static void Main(string[] args)
         {
+            Character<AttackAbility> warrior = new Character<AttackAbility>(Abilities.AttackAbility);
+            Character<MagicAbility> magician = new Character<MagicAbility>(Abilities.MagicAbility);
+            Character<HealAbility> healer = new Character<HealAbility>(Abilities.HealAbility);
+            
+            warrior.UseAbility();
+            magician.UseAbility();
+            healer.UseAbility();
         }
     }
 }
