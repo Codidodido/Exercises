33267ee9234674888320265011e58f2cc34372b3
Codidodido <139787880+Codidodido@users.noreply.github.com>
2023-11-29 13:55:55 +0330

    Create magic ability class script
    -> Inheritance from IAbility interface
    -> Implement UseAbility method

---
+++ Object Oriented Programming/Abilities/Abilities/MagicAbility.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Abilities
+{
+    public class MagicAbility: IAbility
+    {
+        public void UseAbility()
+        {
+            Console.WriteLine("Magic ... .");
+        }
+    }
+}
